67e37c83f5fc87f8163857c60f7a429c6f457fda
nicokinazoko <nicodunois017157@gmail.com>
2022-01-31 15:14:14 +0700

    Update AdminController.php

--- front-end/laravel/app/Http/Controllers/AdminController.php
+++ front-end/laravel/app/Http/Controllers/AdminController.php
@@ -11,8 +11,8 @@ class AdminController extends Controller
         $storage_path = storage_path();
         $data = json_decode(file_get_contents($storage_path . "/data.json"), true);
         $jumlahData = count($data);
-        echo $data[0]['product_name'];
-        dump($data);
+        // echo $data[0]['product_name'];
+        // dump($data);
         return view('home', ['dataBarang' => $data, 'jumlahData' => $jumlahData]);
     }
 }
